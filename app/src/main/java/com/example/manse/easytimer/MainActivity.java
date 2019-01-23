18168ce751c0a116f735d51591d7f4a9dfa3cf25
koreyfm <mansellkf@g.cofc.edu>
2019-01-23 18:55:20 -0500

    January 23rd 2019 7:00pm

--- app/src/main/java/com/example/manse/easytimer/MainActivity.java
+++ app/src/main/java/com/example/manse/easytimer/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.manse.easytimer;
 
+import android.content.DialogInterface;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Button button;
+
+    //public class CountDownTimer(long millisInFuture, long countDownInterval){
+
+
+    //}
+
+
+    public class CountDownTimer extends android.os.CountDownTimer{
+
+
+        public CountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long l) {
+
+        }
+
+        @Override
+        public void onFinish() {
+
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//calls super constructor
         setContentView(R.layout.activity_main);
+
+        //button  = R.id.this.button;
+
+
+        /*
+        button.setOnCLickListener(new View.OnClickListner()){
+
+            @Override
+            public void onClick(View view) {
+
+            }
+
+        }
+        */
+
+
+
     }
+
+
 }
