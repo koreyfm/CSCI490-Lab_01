fb69d35b47de3edbc9d09863e61bc705cc303cfb
koreyfm <mansellkf@g.cofc.edu>
2019-01-27 19:18:16 -0500

    January 27th 2019 7:00pm

--- app/src/main/java/com/example/manse/easytimer/MainActivity.java
+++ app/src/main/java/com/example/manse/easytimer/MainActivity.java
@@ -6,8 +6,64 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+//import android.os.MyCountDownTimer;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CountDownTimer countDownTimer;
+    //private MyCountDownTimer countDownTimer;
+
+    private long timeElapsed;
+    private boolean timerHasStarted = false;
+    private Button startB;
+
+    //private View text;
+    //private View timeElapsedView;
+    private TextView text;
+    private TextView timeElapsedView;
+
+
+    private final long startTime = 50 * 1000;
+    private final long interval = 1 * 1000;
+    public long millisUntilFinished = startTime - timeElapsed;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);//calls super constructor
+        setContentView(R.layout.activity_main);
+        //button  = R.id.this.button;
+
+        startB = (Button) this.findViewById(R.id.button);
+        startB.setOnClickListener(this);
+        text = (TextView) this.findViewById(R.id.timer);
+        timeElapsedView = (TextView) this.findViewById(R.id.timeElapsed);
+        countDownTimer = new CountDownTimer(startTime, interval);
+        text.setText(text.getText() + String.valueOf(startTime));
+
+
+
+        //button  = R.id.this.button;
+
+
+        /*
+        button.setOnCLickListener(new View.OnClickListner()){
+
+            @Override
+            public void onClick(View view) {
+
+            }
+
+        }
+        */
+
+
+
+    }
 
     //Button button;
 
@@ -27,10 +83,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void onTick(long l) {
 
+            text.setText("Time remain: " + millisUntilFinished);
+            timeElapsed = startTime - millisUntilFinished;
+            timeElapsedView.setText("Time Elapsed: " + String.valueOf(timeElapsed));
+
         }
 
         @Override
         public void onFinish() {
+
+            text.setText("Time's up!");
+            timeElapsedView.setText("Time Elapsed: " + String.valueOf(startTime));
 
         }
     }
@@ -39,30 +102,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);//calls super constructor
-        setContentView(R.layout.activity_main);
-
-        //button  = R.id.this.button;
-
-
-        /*
-        button.setOnCLickListener(new View.OnClickListner()){
-
-            @Override
-            public void onClick(View view) {
-
-            }
-
+        if(!timerHasStarted) {
+            countDownTimer.start();
+            timerHasStarted = true;
+            startB.setText("Start");
+        } else {
+            countDownTimer.cancel();
+            timerHasStarted = false;
+            startB.setText("Reset");
         }
-        */
-
-
 
     }
+
+
 
 
 }
